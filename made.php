a9b414b852b76ff7cbd7ba8f24052da99a4e7165
root <root@DESKTOP-2ELDUVN.localdomain>
2020-05-28 22:50:23 +0800

    up

--- made.php
+++ made.php
@@ -2,7 +2,12 @@
 $file = $argv[1];
 //echo 'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \'/mnt/d/下载/'.$file.'\'';
 //exit;
-$avg = exec('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \'/mnt/d/下载/'.$file.'\'');
+$dir = '/mnt/d/下载/';
+if(empty($file)||!file_exists($dir.$file))
+{
+	die('未输入文件名，或者文件不存在！');
+}
+$avg = exec('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \''.$file.'\'');
 $avg = round($avg);
 for($i=300;$i<=$avg-500;$i+=mt_rand(60,300))
 {
